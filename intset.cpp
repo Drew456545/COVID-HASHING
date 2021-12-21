971b30c130a58e0749c6cf4798a824d75d724b4a
Drew Parker <drew456545@gmail.com>
2021-12-21 13:00:19 -0500

    Update intset.cpp

--- intset.cpp
+++ intset.cpp
@@ -1,78 +1,78 @@
-
 #include <iostream>
-#include "locmap.h"
+#include "intset.h"
 using namespace std;
-void free_locs(Location *L)
+Intset::Intset()
 {
-    while (L != NULL) {
-        Location *to_delete = L;
-        L = L->next;
-        delete to_delete;
-    }
+  N = 0;  // start with no elements present
+  M = 4;  // start with a 4-element array 
+  table = new Node *[M]();
 }
-Location *copy_locs(Location *L)
+Intset::~Intset()
 {
-    return L== nullptr ? nullptr : new Location(L->seq, L->index, copy_locs(L->next));
+  free_table();  
 }
-Locmap::Locmap()
-{
-    N = 0;  // start with no elements present
-    M = 100;  // start with a 4-element array
-    table = new Node *[M]();
-}
-Locmap::~Locmap() { free_table(); }
 /* Private function to de-allocate all table memory */
-void Locmap::free_table(void)
+void Intset::free_table(void)
 {
-    for (int i=0; i<M; i++)
-        while (table[i] != NULL) {
-            Node *to_delete = table[i];
-            table[i] = table[i]->next;
-            free_locs(to_delete->locs);
-            delete to_delete;
-        }
-    delete [] table;
+  for (int i=0; i<M; i++)
+    while (table[i] != NULL) {
+      Node *to_delete = table[i];
+      table[i] = table[i]->next;
+      delete to_delete;
+    }
+  delete [] table;
 }
-int Locmap::myhash(int key, int table_size)
+int Intset::myhash(int key, int table_size)
 {
-    return ((unsigned)key * 1791321 + 76543) % table_size;
+  return ((unsigned)key * 1791321 + 76543) % table_size;
 }
-int Locmap::myhash(int key)
+int Intset::myhash(int key)
 {
-    return myhash(key, M);
+  return myhash(key, M);
 }
 /* Return true if key is in the set */
-bool Locmap::find(int key)
+bool Intset::find(int key)
 {
-    for (Node *n = table[myhash(key)]; n != NULL; n = n->next)
-        if (n->key == key) return true;
-    return false;
+  for (Node *n = table[myhash(key)]; n != NULL; n = n->next)
+    if (n->key == key) return true;
+  return false;
 }
-Location *&Locmap::operator[] (int key) {
-    if (!find(key)) insert(key);
-    Node *n = table[myhash(key)];
-    while (n->key != key) n = n->next;
-    return n->locs;
-}
-//Inserts a new key, if it isn't already present
-void Locmap::insert(int key)
+/* Inserts a new key, if it isn't already present */
+void Intset::insert(int key)
 {
-    if (find(key)) return;
-    // Enlarge (by 2x) the underlying array if it isn't large enough to preserve mem
-    if (N == M) {
-        Node **newtbl = new Node *[M*2]();
-        for (int i=0; i<M; i++)
-            for (Node *n = table[i]; n != NULL; n = n->next) {
-                int h = myhash(n->key, M*2);
-                newtbl[h] = new Node(n->key, newtbl[h]);
-                newtbl[h]->locs = copy_locs(n->locs);
-            }
-        free_table();
-        M *= 2;
-        table = newtbl;
-    }
-    N++;
-    int h = myhash(key);
-    table[h] = new Node(key, table[h]);
+  if (find(key)) return;
+  /* Enlarge (by 2x) the underlying array if it isn't large enough... */
+  if (N == M) {
+    Node **newtbl = new Node *[M*2]();
+    for (int i=0; i<M; i++)
+      for (Node *n = table[i]; n != NULL; n = n->next) {
+int h = myhash(n->key, M*2);
+newtbl[h] = new Node(n->key, newtbl[h]);
+      }
+    free_table(); 
+    M *= 2;
+    table = newtbl;
+  }
+  N++;
+  int h = myhash(key);
+  table[h] = new Node(key, table[h]);
+}
+/* Removes a key; does nothing if key not in the set */
+void Intset::remove(int key)
+{
+  if (!find(key)) return;
+  N--;
+  int h = myhash(key);
+  Node *prev = NULL, *to_del = table[h];
+  while (to_del->key != key) { prev = to_del; to_del = to_del->next; }
+  (prev!=NULL ? prev->next : table[h]) = to_del->next;
+  delete to_del;
+}
+void Intset::print(void)
+{
+  for (int i=0; i<M; i++)
+    for (Node *n = table[i]; n != NULL; n = n->next) 
+      cout << n->key << " ";
+  cout << "\n";
 }
 
